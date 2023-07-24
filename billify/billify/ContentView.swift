d6b1e01d24bc56a6a545a9739f775103392a052a
Michael Dadzie <dadziemikke2@gmail.com>
2023-07-24 19:00:47 +0000

    dir updates

--- billify/billify/ContentView.swift
+++ billify/billify/ContentView.swift
@@ -1,14 +1,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmout = 0.0
+    @State private var numOfPeople = 2
+    @State private var tipPercanetage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numOfPeople + 2)
+        let tipSelection = Double(tipPercanetage)
+        
+        let tipValue = checkAmout / 100 * tipSelection
+        let grandTotal = checkAmout + tipValue
+        let amountPerPerson = grandTotal /  peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmout, format:
+                            .currency(code: Locale.current.currency?.identifier ?? "Ghc"))
+                            .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
+                    
+                    Picker("Number of People", selection: $numOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercanetage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "Ghc"))
+                }
+                .navigationTitle("Billify")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
