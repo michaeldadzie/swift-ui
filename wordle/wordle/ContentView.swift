5e29a19af9ba05575cfdf366d744376820f917a8
Michael Dadzie <dadziemikke2@gmail.com>
2023-08-11 22:12:45 +0000

    dir updates

--- wordle/wordle/ContentView.swift
+++ wordle/wordle/ContentView.swift
@@ -7,14 +7,26 @@ struct ContentView: View {
     
     @State private var errorTitle = ""
     @State private var errorMessage = ""
+    @State private var wordCount = 0
+    @State private var score = 0
+    @State private var resetPlay = false
     @State private var showingError = false
+    @State private var textError = false
     
     var body: some View {
         NavigationView {
             List {
                 Section {
-                    TextField("Enter your word", text: $newWord)
-                        .autocapitalization(.none)
+                    VStack(alignment: .leading) {
+                        TextField("Enter your word", text: $newWord)
+                            .autocapitalization(.none)
+                            .onSubmit(addNewWord)
+                        
+                        if (textError) {
+                            Text("Input must be at least 3 characters")
+                                .foregroundColor(.red)
+                        }
+                    }
                 }
                 
                 Section {
@@ -25,42 +37,62 @@ struct ContentView: View {
                         }
                     }
                 }
+                
+                Section {
+                    Text("Score: \(score)")
+                }
             }
             .navigationTitle(rootWord)
-            .onSubmit(addNewWord)
             .onAppear(perform: startGame)
-            .alert(errorTitle,isPresented: $showingError) {
+            .toolbar {
+                Button("Change", action: startGame)
+            }
+            .alert(errorTitle, isPresented: $showingError) {
                 Button("OK", role: .cancel) {}
             } message: {
                 Text(errorMessage)
+            }
+            .alert("Game Over", isPresented: $resetPlay) {
+                Button("Restart", action: startGame)
+            } message: {
+                Text("Your score is: \(score)")
             }
         }
     }
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count > 0 else { return }
+        guard answer.count > 0 else {
+            reset()
+            return
+        }
         
         guard isOriginal(word: answer) else {
+            reset()
             wordError(title: "Word used already", message: "Be more original")
             return
         }
         
         guard isPossible(word: answer) else {
+            reset()
             wordError(title: "Word not possible", message: "You can't spell this word from '\(rootWord)!'")
             return
         }
         
         guard isReal(word: answer) else {
+            reset()
             wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
             return
         }
             
         withAnimation { usedWords.insert(answer, at: 0) }
+        reset()
         newWord = ""
+        score += 1
     }
     
     func startGame() {
+        resetGame()
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allwords = startWords.components(separatedBy: "\n")
@@ -69,6 +101,22 @@ struct ContentView: View {
             }
         }
         fatalError("Could not load start.txt from bundle")
+    }
+    
+    func reset() {
+        wordCount += 1
+        if (wordCount == 8) {
+            wordCount = 0
+            usedWords = []
+            showingError = false
+            resetPlay = true
+        }
+    }
+    
+    func resetGame() {
+        score = 0
+        usedWords = []
+        newWord = ""
     }
     
     func isOriginal(word: String) -> Bool {
@@ -87,6 +135,7 @@ struct ContentView: View {
         }
         return true
     }
+    
     
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
